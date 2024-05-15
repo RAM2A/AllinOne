650eec89b6bc9365c2d81dc360692cf1699b140c
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-05-15 10:19:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6731306687:AAH7FhCo9vBpM_PX2blym_RgbEKnWoCAcNA")
-    API_ID = int(os.environ.get("API_ID", "22609670"))
-    API_HASH = os.environ.get("API_HASH", "3506d8474ad1f4f5e79b7c52a5c3e88d")
-    AUTH_USERS = "5934830127,6981453498"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6700540841:AAEzEG75XEQXqfTGmIvy136zVclAUBxQKOI")
+    API_ID = int(os.environ.get("API_ID", "24665357"))
+    API_HASH = os.environ.get("API_HASH", "beb7e4b83ada668fa85f9a9b56338f1d")
+    AUTH_USERS = "1717511106"
 
